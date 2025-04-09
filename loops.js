738471c7fc25b354d421987c0b2a0af5ab39663f
Sai2007-pixel <saichandrapoludasu0@gmail.com>
2025-04-09 14:41:20 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,22 @@
+console.log("for loop");
+for(i=1; i<=10; i++){
+    console.log(i);
+}
+
+console.log("while loop");
+let A
+A=1
+while(A=10){
+console.log(A);
+A++
+}
+
+console.log("do while");
+let n=1
+do{
+    console.log(n)
+    n++
+}
+while(n<=20)
+
+
